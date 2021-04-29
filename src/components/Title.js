60d5367a3c6d46d5c449e15680b1027be8a25d56
minjae3292 <minjae3292@tradlinx.com>
2021-04-29 18:02:21 +0900

    1. title modify

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import '../styles/Title.css'
 
 function Title(props) {
     return (
-        <div>
-            <h2> Rule Note </h2>
+        <div className='Title'>
+            <h2 className='Title-Name'> Rule Note </h2>
         </div>
     );
 }
